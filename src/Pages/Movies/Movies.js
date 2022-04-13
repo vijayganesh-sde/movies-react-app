3807d48c4750ea92dcb8a9e107f83bde43345b2e
vijayganesh-sde <93527877+vijayganesh-sde@users.noreply.github.com>
2022-04-13 22:24:22 +0530

    2nd commit

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,4 +1,31 @@
-const Movies = () => {
-  return <div className="title">movies</div>;
-};
-export default Movies;
+import axios from "axios";
+import { Component, React } from "react";
+export default class PersonList extends Component {
+  state = {
+    persons: []
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://api.themoviedb.org/4/account/Vijay@03/movie/recommendations?page=1`
+      )
+      .then((res) => {
+        const persons = res.data;
+        this.setState({ persons });
+        console.log(this.state.persons);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          {this.state.persons.map((person) => (
+            <li key={person.id}>{person.name}</li>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
